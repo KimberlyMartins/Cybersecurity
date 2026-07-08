9c6540659e3066536a8aaa8d8f48f5520e651628
bowie <kiiim.martins@gmail.com>
2026-07-08 15:43:01 -0300

    Update 1-foundations_cybersecurity.rb

--- 1-foundations_cybersecurity.rb
+++ 1-foundations_cybersecurity.rb
@@ -11,21 +11,21 @@ A prática de manter a infraestrutura de rede de uma organização protegida con
 A capacidade de uma organização de gerenciar sua defesa de recursos e dados críticos e reagir a mudanças 
 
                            AMEAÇA
-Qualquer circunstância ou evento que possa afetar negativamente os recursos
-Ameaça interna: Um funcionário ou ex-funcionário, um fornecedor externo ou um parceiro confiável que represente um Risco à segurança
-Ator da ameaça: Qualquer pessoa ou grupo que apresente um Risco à segurança
+Qualquer circunstância ou evento que possa afetar negativamente os recursos.
+Ameaça interna: Um funcionário ou ex-funcionário, um fornecedor externo ou um parceiro confiável que represente um Risco à segurança.
+Ator da ameaça: Qualquer pessoa ou grupo que apresente um Risco à segurança.
 
              INFORMAÇÕES DE IDENTIFICAÇÃO PESSOAL (PII) 
-Qualquer informação usada para inferir a identidade de um indivíduo
+Qualquer informação usada para inferir a identidade de um indivíduo.
 
         INFORMAÇÕES SENSÍVEIS DE IDENTIFICAÇÃO PESSOAL (SPII) 
-Um tipo específico de PII que se enquadra em diretrizes de manuseio mais rigorosas
+Um tipo específico de PII que se enquadra em diretrizes de manuseio mais rigorosas.
 
                      HABILIDADES TÉCNICAS 
 Habilidades que exigem conhecimento de ferramentas, procedimentos e políticas específicas.
 
                      HABILIDADES TRANSFERÍVEIS 
-Habilidades de outras áreas que podem ser aplicadas a diferentes carreiras
+Habilidades de outras áreas que podem ser aplicadas a diferentes carreiras.
 
 
 
